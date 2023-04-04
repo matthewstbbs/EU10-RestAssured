26e9f76fd1fc2a6f08aa7de8b335a406ab9d908d
Matthew Stubbs <mattstbbs@outlook.com>
2023-04-04 12:58:17 +0300

    negative task

--- src/test/java/com/cydeo/day2/SpartanNegativeGetTest_3.java
+++ src/test/java/com/cydeo/day2/SpartanNegativeGetTest_3.java
@@ -11,7 +11,7 @@ import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class SpartanNegativeGetTest {
+public class SpartanNegativeGetTest_3 {
 
     @BeforeAll
     public static void init(){
